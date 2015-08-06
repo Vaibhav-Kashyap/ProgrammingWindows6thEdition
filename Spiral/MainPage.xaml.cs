b02947e5a249f19dd39f38d0eabc60b7bb07f1d7
wildcardjoker <the.pctechnician+github@gmail.com>
2015-08-06 21:35:21 +0800

    Add Spiral project.
    Generate a spiral pattern using a Polyline and a loop to generate
    points.

---
+++ Spiral/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace Spiral
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            for (int angle = 0; angle < 3600; angle++)
+            {
+                double radians = Math.PI*angle/180;
+                double radius = angle / 10;
+                double x = 360 + radius * Math.Sin(radians);
+                double y = 360 - radius * Math.Cos(radians);
+                Polyline.Points.Add(new Point(x, y));
+            }
+        }
+    }
+}
